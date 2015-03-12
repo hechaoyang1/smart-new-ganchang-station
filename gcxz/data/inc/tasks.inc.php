7f65413609c17822a00fdad2822a9fb15eb91f72
xucaibing <xucaibing@Lenovo-PC>
2015-03-12 11:53:38 +0800

    修改忽略文件同步问题

--- gcxz/data/inc/tasks.inc.php
+++ gcxz/data/inc/tasks.inc.php
@@ -5,8 +5,8 @@ return array (
   array (
     'cycle' => 'custom',
     'interval' => 3600,
-    'due_time' => 1426129568,
-    'last_time' => 1426125968,
+    'due_time' => 1426133176,
+    'last_time' => 1426129576,
   ),
 );
 
