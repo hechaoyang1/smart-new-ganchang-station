1df33ffe94a81da33989db4d19c43a2bf0d08b0a
xucaibing <xucaibing@Lenovo-PC>
2015-03-24 09:57:23 +0800

    验货失败时 不修改卖家退货原因

--- gcxz/app/search.app.php
+++ gcxz/app/search.app.php
@@ -46,7 +46,7 @@ class SearchApp extends MallbaseApp
 				'count' => true 
 		) );
 		if($param['keyword'] && !$goods_list){
-			$this->show_warning ( '没有找到与'.$_GET['keyword'].'相关商品' );
+			$this->show_warning ( '没有找到相关商品' );
 		}
 		$page ['item_count'] = $goods_mod->_last_query_count;
 		foreach ( $goods_list as $key => $goods ) {
