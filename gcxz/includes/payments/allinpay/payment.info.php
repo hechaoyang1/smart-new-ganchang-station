1c11e5130d01dbd308a00e513ac87d9d72e72357
liaoying <admin@liaoy>
2015-03-24 09:55:08 +0800

    支付描述

--- gcxz/includes/payments/allinpay/payment.info.php
+++ gcxz/includes/payments/allinpay/payment.info.php
@@ -2,8 +2,8 @@
 
 return array(
     'code'      => 'allinpay',
-    'name'      => '通联支付',
-    'desc'      => '通联第三方支付',
+    'name'      => '在线支付',
+    'desc'      => '在线支付',
     'is_online' => '1',
     'version'   => '1.0',
     'currency'  => '人民币',
