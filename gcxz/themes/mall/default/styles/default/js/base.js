05d80eda33d4b2760116d42d5c414556e9ca910d
zjj <zjj@zjj-PC>
2015-03-27 17:33:07 +0800

    首页、商品详情页增加右侧工具栏

--- gcxz/themes/mall/default/styles/default/js/base.js
+++ gcxz/themes/mall/default/styles/default/js/base.js
@@ -48,4 +48,73 @@ gcxz.shoppingCar = function(ele){
 		}
 	}
 };
+
+//收藏夹
+gcxz.favorite = function(ele){
+	if(gcxz.favorite.isopen)return;
+	gcxz.favorite.isopen = true;
+	$.post(urls.favorite,function(data){
+		 	ele = $(ele);
+			var eleH = ele.height(),
+			offsetHeight = $(document).scrollTop(),
+			eleTop = ele.offset().top,
+			centerHeight = eleTop - offsetHeight + eleH/2,
+			appendHtml = $(data).hide().appendTo('body'),
+			appendHtmlHeight = appendHtml.height();
+		appendHtml.css({
+			top:centerHeight - appendHtmlHeight/2-20
+		}).show();
+
+		setTimeout(function(){
+			$(document).on('click',removeAppendHtml);
+		});
+	});
+	
+	function removeAppendHtml(e){
+		var t = $(e.target);
+		var modal = t.closest('.modal');
+		var modal2 =$('.modal');
+		if(modal2.length != 0 && modal.length === 0){
+			modal2.remove();
+			$(document).off('click',removeAppendHtml);
+			gcxz.favorite.isopen = false;
+		}
+	}
+};
+gcxz.drop_good = function(store_id, rec_id,obj) {
+	var li = $(obj).parents('li');
+	var shop = $(obj).parents('.shop');
+	$.getJSON('index.php?app=cart&act=drop&rec_id=' + rec_id, function(result){
+        if(result.done){
+        	 //删除成功
+            if(result.retval.cart.quantity == 0){
+            	$('.rightTools').click();    //刷新
+            }
+            else{
+            	/*如果还有其他商品，移除该商品*/
+            	if(li.siblings('li').length>0){
+	                li.remove();        //移除
+            	}else{
+            	/*如果没有其他商品，移除店铺*/
+            		shop.remove(); 
+            	}
+            }
+        }
+    });
+}
+gcxz.drop_favorite_good = function(goods_id,obj) {
+	var li = $(obj).parents('li');
+	var shop = $(obj).parents('.shop');
+	$.getJSON('index.php?app=my_favorite&act=ajax_drop&item_id=' + goods_id, function(result) {
+		if (result == 1) {
+			/*如果还有其他商品，移除该商品*/
+			if (li.siblings('li').length > 0) {
+				li.remove(); //移除
+			} else {
+				/*如果没有其他商品，移除店铺*/
+				$('.rightTools').click(); //刷新
+			}
+		}
+	});
+}
 gcxz.shoppingCar.isopen = false;
