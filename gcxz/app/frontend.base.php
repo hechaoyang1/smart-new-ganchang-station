a99e40e4ad577bef0e32ef58b73ad84f390d10ad
xucaibing <xucaibing@Lenovo-PC>
2015-03-31 17:47:02 +0800

    修改缓存时间

--- gcxz/app/frontend.base.php
+++ gcxz/app/frontend.base.php
@@ -1060,7 +1060,7 @@ class StorebaseApp extends FrontendApp
                     $store['functions'][$v] = $v;
                 }
             }
-            $cache_server->set($key, $store, 60);
+            $cache_server->set($key, $store, 1800);
         }
         return $store;
     }
