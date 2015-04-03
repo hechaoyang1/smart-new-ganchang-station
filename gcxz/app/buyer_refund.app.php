b7858aa3cc03a71eb850067a176ed7270220ef45
liaoying <admin@liaoy>
2015-04-03 10:22:03 +0800

    退款流程修改，已付款到订单完成之前都能申请退款

--- gcxz/app/buyer_refund.app.php
+++ gcxz/app/buyer_refund.app.php
@@ -88,7 +88,7 @@ class Buyer_refundApp extends MemberbaseApp
             return;
         }
         // 订单状态校验
-        if ($order['status'] != ORDER_PENDED && $order['status'] != ORDER_ACCEPTED) {
+        if ($order['status'] != ORDER_PENDED && $order['status'] != ORDER_ACCEPTED && $order['status'] != ORDER_REQUEST && $order['status'] != ORDER_RESPONSE && $order['status'] != ORDER_SHIPPED) {
             $this->show_warning ( 'wrong_status' );
             return;
         }
