cb76a0738652bac61276821df8b81fd48bd8382e
zjj <zjj@zjj-PC>
2015-04-13 20:19:38 +0800

    土特产列表页增加置顶商品缩略图展示

--- gcxz/includes/models/regiondescription.model.php
+++ gcxz/includes/models/regiondescription.model.php
@@ -15,14 +15,26 @@ class RegiondescriptionModel extends BaseModel {
 								re.region_id = ' . $id );
 	}
 	function get_List($id) {
-		return $this->db->getAll ( 'SELECT
-								erd.id,re.region_id,re.region_name,erd.description,erd.default_image,re.sort_order,erd.if_show
-							FROM
-								ecm_region_description erd
-							RIGHT JOIN ecm_region re ON erd.region_id = re.region_id
-							WHERE
-								re.parent_id = ' . $id );
-	}
+        $list = $this->db->getAll(
+                'SELECT
+                	erd.id,
+                	re.region_id,
+                	re.region_name,
+                	erd.description,
+                	erd.default_image,
+                	re.sort_order,
+                	erd.if_show,
+                	g.default_image as goods_image
+                FROM
+                	ecm_region re
+                LEFT JOIN ecm_region_description erd ON erd.region_id = re.region_id
+                LEFT JOIN ecm_region_goods rg ON re.region_id = rg.region_id
+                AND rg.type = 2
+                LEFT JOIN ecm_goods g ON rg.goods_id = g.goods_id
+                WHERE
+                	re.parent_id = ' . $id);
+        return $list;
+    }
 }
 
 ?>
