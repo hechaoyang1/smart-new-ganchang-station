5fcbf00a0404a9f2a5179fbbca5867e32f6cfde6
liaoying <admin@liaoy>
2015-04-15 16:46:22 +0800

    土特产专题

--- gcxz/themes/mall/default/styles/default/js/ttc_2.js
+++ gcxz/themes/mall/default/styles/default/js/ttc_2.js
@@ -5,36 +5,46 @@ var mopt = {
 	liheight : 46
 }
 $(function() {
-	$("svg circle").mousemove(function() {
-		show = false;
-		var rid = this.id.substr(2);
-		clearTimeout(showtimer);
-		showtimer = setTimeout("showtj(" + rid + ",11" + ")", 400);
-	}).mouseover(function() {
-		clearTimeout(showtimer);
-		var rid = this.id.substr(2);
-		hidetop(rid);
-		active(rid, true);
-	}).mouseout(
-			function(e) {
+	$("svg circle")
+			.mousemove(function() {
+				show = false;
+				var rid = this.id.substr(2);
+				clearTimeout(showtimer);
+				showtimer = setTimeout("showtj(" + rid + ",11" + ")", 400);
+			})
+			.mouseover(function() {
 				clearTimeout(showtimer);
 				var rid = this.id.substr(2);
-				if (!$(e.toElement).is(".native_map_2")
-						&& $(e.toElement).parents(".native_map_2").length == 0) {
-				showtop();
-					// $("#tc_"+rid).hide();
-					mopt.tctimer[rid] = setTimeout('$("#tc_' + rid + '").hide()',
-							800);
-				}
-				active(rid, false);
-			}).click(
-			function() {
-				if ($(this).data("is_show")) {
-					var rid = this.id.substr(2);
-					window.location.href = SITE_URL
-							+ "/index.php?app=special&rid=" + rid;
-				}
-			});
+				hidetop(rid);
+				active(rid, true);
+			})
+			.mouseout(
+					function(e) {
+						clearTimeout(showtimer);
+						var rid = this.id.substr(2);
+						if (!$(e.toElement).is(".native_map_2")
+								&& $(e.toElement).parents(".native_map_2").length == 0) {
+							showtop();
+							// $("#tc_"+rid).hide();
+							mopt.tctimer[rid] = setTimeout('$("#tc_' + rid
+									+ '").hide()', 800);
+						}
+						if (!$(e.toElement).is(".native_map_1")&&!$(e.toElement).is(".native_map_2")) {
+							$("div.tjsp.not_active .native_map_1").stop().animate({
+								opacity : "0"
+							}, 200, null, function() {
+								$(this).hide();
+							});
+						}
+						active(rid, false);
+					}).click(
+					function() {
+						if ($(this).data("is_show")) {
+							var rid = this.id.substr(2);
+							window.location.href = SITE_URL
+									+ "/index.php?app=special&rid=" + rid;
+						}
+					});
 	$(".native_names1 .native_namesli1").mouseenter(function() {
 		var rid = this.id.substr(2);
 		active(rid, false);
@@ -46,7 +56,7 @@ $(function() {
 	});
 })
 function showtj(rid, lc) {
-	if(!$("#m_"+rid).data("is_show")){
+	if (!$("#m_" + rid).data("is_show")) {
 		return;
 	}
 	show = true;
@@ -64,6 +74,9 @@ function showtj(rid, lc) {
 					$("#tc_" + rid).mouseleave(function(e) {
 						// if (!$(e.toElement).is("#m_" + rid)) {
 						$(this).hide();
+						if ($(e.toElement).is(":not(svg circle)")) {
+							showtop();
+						}
 						// mopt.tctimer = setTimeout('$("#tc_' + rid
 						// + '").hide()', 800);
 						// }
@@ -89,8 +102,7 @@ function active(rid, flag) {
 		}
 	}
 }
-function showtop(){
-	hidetop();
+function showtop() {
 	$("div.tjsp:not(.not_active) .native_map_1").stop().show().animate({
 		opacity : "1"
 	}, 500);
@@ -116,26 +128,42 @@ function hidetop(rid) {
 $(function() {
 	cbpBGSlideshow.init();
 });
-$(document).ready(function() {
-	for ( var k in regions) {
-		$("#m_" + regions[k].region_id).data("is_show", regions[k].if_show);
-	}
-	$("svg circle").each(function(){
-		var $this=$(this);
-		var rid=this.id.substr("2");
-		if(!$this.data("is_show")){
-			$("#container>div").append('<div id="tj_'+rid+'" class="tjsp not_active" style="display:none;"><div class="native_map_1 native_map_1_no" style="display:none;position: absolute; top:-112px; left:-6px;"></div></div>');
-		}
-	});
-	
-	$("div.tjsp").each(function() {
-		var $this = $(this);
-		var id = "#m_" + this.id.substr(3);
-		var location = $(id).attr("lc").split(",");
-		$this.css({
-			position : "absolute",
-			top : location[0] + "px",
-			left : location[1] + "px"
-		});
-	}).show();
-});
+$(document)
+		.ready(
+				function() {
+					for ( var k in regions) {
+						$("#m_" + regions[k].region_id).data("is_show",
+								regions[k].if_show);
+					}
+					$("svg circle")
+							.each(
+									function() {
+										var $this = $(this);
+										var rid = this.id.substr("2");
+										if (!$this.data("is_show")) {
+											$("#container>div")
+													.append(
+															'<div id="tj_'
+																	+ rid
+																	+ '" class="tjsp not_active" style="display:none;"><div class="native_map_1 native_map_1_no" style="display:none;position: absolute; top:-112px; left:-6px;"></div></div>');
+										}
+									});
+					$(".native_map_1").mouseenter(function() {
+						$(".native_map_1").not(this).hide();
+					}).mouseleave(function(e) {
+						if ($(e.toElement).is(":not(svg circle)")) {
+							hidetop();
+						}
+						showtop();
+					});
+					$("div.tjsp").each(function() {
+						var $this = $(this);
+						var id = "#m_" + this.id.substr(3);
+						var location = $(id).attr("lc").split(",");
+						$this.css({
+							position : "absolute",
+							top : location[0].trim() + "px",
+							left : location[1].trim() + "px"
+						});
+					}).show();
+				});
