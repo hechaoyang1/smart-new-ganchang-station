2d8846cef9c1edd951debf541b058aa40ea68a3a
liaoying <admin@liaoy>
2015-04-15 13:45:45 +0800

    土特产专题

--- gcxz/app/special.app.php
+++ gcxz/app/special.app.php
@@ -6,6 +6,7 @@
  */
 class SpecialApp extends MallbaseApp {
 	var $m;
+	var $renshou = 511421;
 	function __construct() {
 		$this->m = m ( 'region' );
 		parent::__construct ();
@@ -14,7 +15,7 @@ class SpecialApp extends MallbaseApp {
 		$region_id = intval ( $_GET ['rid'] );
 		if (! $region_id) {
 			// 默认展示整个仁寿
-			$region_id = 511421;
+			$region_id = $this->renshou;
 		}
 		// 地区数据
 		$data = $this->_get_regions ( $region_id );
@@ -23,14 +24,19 @@ class SpecialApp extends MallbaseApp {
 		$imgs = $this->m->db->getAll ( "SELECT bg_image,description FROM ecm_region_bg_image where region_id={$region_id}" );
 		$file = ROOT_PATH . "/themes/mall/default/styles/default/svg/m_{$region_id}.svg";
 		if (file_exists ( $file )) {
-			$map = file_get_contents ($file);
+			$map = file_get_contents ( $file );
 			$this->assign ( "map", $map );
 		}
-		$this->assign("more",count($data)>10);
+		$this->_cur_top ( $region_id );
+		$this->assign ( "more", count ( $data ) > 10 );
 		$this->assign ( "regions", $data );
-		$this->assign("regionsjson",json_encode($data));
+		$this->assign ( "regionsjson", json_encode ( $data ) );
 		$this->assign ( "goods", $goods );
+		$this->assign("region_id",$region_id);
 		$this->assign ( "imgs", $imgs );
+		if ($region_id == $this->renshou) {
+			$this->assign ( "is_area", true );
+		}
 		$this->display ( "ttc.html" );
 	}
 	
@@ -72,21 +78,58 @@ class SpecialApp extends MallbaseApp {
 		$this->display ( "tc.html" );
 	}
 	/**
+	 * 面包屑
+	 */
+	function _cur_top($rid) {
+		if ($rid == $this->renshou) {
+			$tops [] = array (
+					"rid" => $this->renshou,
+					"name" => '仁寿县',
+					'end' => true 
+			);
+			$this->assign ( 'top', $tops );
+			return;
+		}
+		$data = $this->m->db->getAll ( "SELECT r1.region_id as r1, r1.region_name as n1, r2.region_id as r2, r2.region_name as n2, r3.region_id as r3, r3.region_name as n3 FROM ecm_region r1 LEFT JOIN ecm_region r2 ON r1.parent_id = r2.region_id LEFT JOIN ecm_region r3 ON r2.parent_id = r3.region_id WHERE r1.region_id = $rid" );
+		$data = current ( $data );
+		$tops [] = array (
+				"rid" => $data ['r1'],
+				"name" => $data ['n1'],
+				'end' => true 
+		);
+		$tops [] = array (
+				"rid" => $data ['r2'],
+				"name" => $data ['n2'] 
+		);
+		if ($data ['r2'] == $this->renshou) {
+			$tops = array_reverse ( $tops );
+			$this->assign ( 'top', $tops );
+			return;
+		}
+		$tops [] = array (
+				"rid" => $data ['r3'],
+				"name" => $data ['n3'],
+				'end' => true 
+		);
+		$tops = array_reverse ( $tops );
+		$this->assign ( 'top', $tops );
+	}
+	/**
 	 * 获得下级地区列表
 	 *
 	 * @param unknown $rid        	
 	 * @return Ambigous <boolean, unknown>
 	 */
 	function _get_regions($rid) {
-// 		$cache_server = & cache_server ();
-// 		$data = $cache_server->get ( 'regions_pid_' . $rid );
-// 		if ($data === false) {
-			$data = $this->m->db->getAll ( "select r.region_id,r.region_name,rd.if_show from ecm_region r left join ecm_region_description rd on rd.region_id =r.region_id where r.parent_id={$rid} and rd.if_show=1" );
-			// 缓存一天
-// 			if ($data) {
-// 				$cache_server->set ( 'regions_pid_' . $rid, $data, 24 * 60 * 60 );
-// 			}
-// 		}
+		// $cache_server = & cache_server ();
+		// $data = $cache_server->get ( 'regions_pid_' . $rid );
+		// if ($data === false) {
+		$data = $this->m->db->getAll ( "select r.region_id,r.region_name,rd.if_show from ecm_region r left join ecm_region_description rd on rd.region_id =r.region_id where r.parent_id={$rid} and rd.if_show=1" );
+		// 缓存一天
+		// if ($data) {
+		// $cache_server->set ( 'regions_pid_' . $rid, $data, 24 * 60 * 60 );
+		// }
+		// }
 		return $data;
 	}
 }
